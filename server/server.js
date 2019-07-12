4dbbfb823bad179c55adaa40a40bca2357ce5045
damanm24 <dmulye2@illinois.edu>
2019-07-12 13:00:22 -0700

    Version 1.0

--- server/server.js
+++ server/server.js
@@ -31,9 +31,9 @@ app.use(bodyParser.json())
 if (process.env.NODE_ENV === 'production') {
     const path = require('path')
     console.log('YOU ARE IN THE PRODUCTION ENV')
-    app.use(express.static('client/build'));
+    app.use('/static', express.static(path.join(__dirname, '../build/static')))
     app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../build/'))
     })
 }
 
